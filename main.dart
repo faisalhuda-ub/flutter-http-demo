33be9f7c90f051c068fca2b8dff07c4d6cee1caa
faisalhuda-ub <faisalhuda.es@gmail.com>
2020-12-03 19:04:45 +0700

    push pertama

---
+++ main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:http_demo/helper/api_helper.dart';
+import 'package:http_demo/model/Album.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Http library',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Penerapan http library'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
